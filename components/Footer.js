96953ca0616a42a3990a46794fd9ff475d296ffa
Elton Quek <quekmkelton@gmail.com>
2023-02-26 01:20:27 +0800

    Formatting and text

--- components/Footer.js
+++ components/Footer.js
@@ -1,45 +1,45 @@
-import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io";
-import { Container } from "@mui/system";
-import styles from "../styles/Home.module.css";
+import { IoLogoGithub, IoLogoLinkedin } from 'react-icons/io';
+import { Container } from '@mui/system';
+import styles from '../styles/Home.module.css';
 
 const Footer = () => (
-  <footer className={styles.footer}>
-    <Container>
-      I created this website from scratch, feel free to look at my GitHub for
-      the code or my other socials
-      <br />
-      <SocialNav />
-    </Container>
-  </footer>
+    <footer className={styles.footer}>
+        <Container>
+            I created this website from scratch, feel free to look at my GitHub
+            for the code or my other socials
+            <br />
+            <SocialNav />
+        </Container>
+    </footer>
 );
 
 export default Footer;
 
 const SocialNav = () => (
-  <>
-    {socialLinks.map(([name, link]) => {
-      const SocialIcon = icons[name];
-      return (
-        <a
-          key={name}
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.socialNav}
-        >
-          <SocialIcon />
-        </a>
-      );
-    })}
-  </>
+    <>
+        {socialLinks.map(([name, link]) => {
+            const SocialIcon = icons[name];
+            return (
+                <a
+                    key={name}
+                    href={link}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className={styles.socialNav}
+                >
+                    <SocialIcon />
+                </a>
+            );
+        })}
+    </>
 );
 
 const icons = {
-  linkedin: IoLogoLinkedin,
-  github: IoLogoGithub,
+    linkedin: IoLogoLinkedin,
+    github: IoLogoGithub,
 };
 
 const socialLinks = [
-  ["linkedin", "https://www.linkedin.com/in/eltonquek/"],
-  ["github", "https://github.com/EltonQuekMK"],
+    ['linkedin', 'https://www.linkedin.com/in/eltonquek/'],
+    ['github', 'https://github.com/EltonQuekMK'],
 ];
--- components/Header.js
+++ components/Header.js
@@ -1,50 +1,53 @@
-import React, { useEffect, useState, useCallback } from "react";
-import styles from "../styles/Home.module.css";
-import { Container } from "@mui/system";
-import Logo from "../components/Logo.js";
+import React, { useEffect, useState, useCallback } from 'react';
+import styles from '../styles/Home.module.css';
+import { Container } from '@mui/system';
+import Logo from '../components/Logo.js';
 
 const Header = () => (
-  <>
-    <Container>
-      <Logo/>
+    <>
+        <Container>
+            <Logo />
 
-      <div className={styles.bigWords}>
-        <div>Elton Quek</div>
-        <TextAnimation />
-      </div>
-      <BackgroundAnimation />
-    </Container>
-  </>
+            <div className={styles.bigWords}>
+                <div>I'm <span className={styles.name}>Elton Quek</span></div>
+                <TextAnimation />
+            </div>
+            <BackgroundAnimation />
+        </Container>
+    </>
 );
 
 export default Header;
 
 export function TextAnimation() {
-  const words = [
-    "Software developer",
-    "Caffeine aficionado",
-    "Coding monkey",
-    "Part-time gymrat",
-    "Full-time debugger",
-  ];
-  const [index, setIndex] = useState(0);
+    const words = [
+        'Software developer',
+        'Caffeine aficionado',
+        'Coding...',
+        'Part-time gymrat',
+        'Full-time debugger',
+    ];
+    const [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prevIndex) => {
-        // reset index if current index is greater than array size
-        return prevIndex + 1 < words.length ? prevIndex + 1 : 0;
-      });
-    }, 3000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => {
+                // reset index if current index is greater than array size
+                return prevIndex + 1 < words.length ? prevIndex + 1 : 0;
+            });
+        }, 3000);
 
-    return () => clearInterval(interval);
-  });
-  return (
-    <div key={words[index]} className={styles.textAnimation}>
-      {words[index]}
-    </div>
-  );
+        return () => clearInterval(interval);
+    });
+    return (
+        <div
+            key={words[index]}
+            className={styles.textAnimation}
+        >
+            {words[index]}
+        </div>
+    );
 }
 const BackgroundAnimation = () => {
-  return <></>;
+    return <></>;
 };
