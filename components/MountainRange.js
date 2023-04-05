f875bc027d9d6339f31c9b50e8e7aa21c8d0e2e8
Elton Quek <quekmkelton@gmail.com>
2023-04-05 17:47:52 +0800

    Remove initial path dot

--- components/MountainRange.js
+++ components/MountainRange.js
@@ -43,8 +43,8 @@ export default function MountainRange() {
                 animate='visible'
             >
                 <motion.path
-                    initial={{ pathLength: 0 }}
-                    animate={{ pathLength: 1 }}
+                    initial={{ pathLength: 0, opacity: 0 }}
+                    animate={{ pathLength: 1, opacity: 1 }}
                     transition={transition}
                     strokeWidth={20}
                     stroke='#0066FF'
